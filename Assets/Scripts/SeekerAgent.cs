a400449ed9cb475fb71f92404c16d7f93b8fa9d1
Leonardo Tagliati <leotagliati56@gmail.com>
2026-07-16 15:29:09 -0300

    ninja

--- Assets/Scripts/SeekerAgent.cs
+++ Assets/Scripts/SeekerAgent.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
@@ -6,50 +7,48 @@ using Unity.MLAgents.Sensors;
 
 public class SeekerAgent : Agent
 {
-    [SerializeField] private Transform _targetTransform;
     [SerializeField] private float _moveSpeed = 0.5f;
     [SerializeField] private float _rotationSpeed = 200f;
-
     [SerializeField] private Renderer _renderer;
-    [SerializeField] private float _episodeEndDelay = 0.5f;
 
+    [SerializeField] private float _cellSize = 1f;
+    [SerializeField] private float _explorationReward = 0.02f;
+    [SerializeField] private float _wallCollisionPenalty = 0.5f;
+    [SerializeField] private float _wallFlashDuration = 0.2f;
+
+    private Rigidbody _rigidbody;
+    private readonly HashSet<Vector2Int> _visitedCells = new HashSet<Vector2Int>();
     private int _currentEpisode = 0;
     private float _cumulativeReward = 0f;
-    private bool _episodeEnding = false;
 
     public override void Initialize()
     {
         Debug.Log("Agent initialized");
 
+        _rigidbody = GetComponent<Rigidbody>();
         _currentEpisode = 0;
         _cumulativeReward = 0f;
     }
+
     public override void OnEpisodeBegin()
     {
         Debug.Log("Episode started");
 
         _currentEpisode++;
         _cumulativeReward = 0f;
-        _episodeEnding = false;
         _renderer.material.color = Color.blue;
 
-        SpawnObjects();
+        _visitedCells.Clear();
+        ResetAgentPose();
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        Debug.Log("Collecting observations");
-
-        float goalPosX_normalized = (_targetTransform.localPosition.x + 5f) / 10f;
-        float goalPosZ_normalized = (_targetTransform.localPosition.z + 5f) / 10f;
-
         float agentPosX_normalized = (transform.localPosition.x + 5f) / 10f;
         float agentPosZ_normalized = (transform.localPosition.z + 5f) / 10f;
 
         float agentRotation_normalized = (transform.localRotation.eulerAngles.y / 360f) * 2f - 1f;
 
-        sensor.AddObservation(goalPosX_normalized);
-        sensor.AddObservation(goalPosZ_normalized);
         sensor.AddObservation(agentPosX_normalized);
         sensor.AddObservation(agentPosZ_normalized);
         sensor.AddObservation(agentRotation_normalized);
@@ -57,11 +56,14 @@ public class SeekerAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        Debug.Log("Action received");
-
         MoveAgent(actions.DiscreteActions);
 
-        AddReward(-2f / MaxStep);
+        if (MaxStep > 0)
+        {
+            AddReward(-2f / MaxStep);
+        }
+
+        RewardExplorationIfNewCell();
 
         _cumulativeReward += GetCumulativeReward();
     }
@@ -73,61 +75,47 @@ public class SeekerAgent : Agent
         switch (action)
         {
             case 1:
-                transform.position += transform.forward * _moveSpeed * Time.deltaTime;
+                _rigidbody.MovePosition(_rigidbody.position + transform.forward * _moveSpeed * Time.fixedDeltaTime);
                 break;
             case 2: // rotate left
-                transform.Rotate(Vector3.up, -_rotationSpeed * Time.deltaTime);
+                _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(0f, -_rotationSpeed * Time.fixedDeltaTime, 0f));
                 break;
             case 3: // rotate right
-                transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
+                _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(0f, _rotationSpeed * Time.fixedDeltaTime, 0f));
                 break;
         }
     }
 
-    private void SpawnObjects()
+    private void RewardExplorationIfNewCell()
+    {
+        Vector3 pos = transform.localPosition;
+        var cell = new Vector2Int(Mathf.FloorToInt(pos.x / _cellSize), Mathf.FloorToInt(pos.z / _cellSize));
+
+        if (_visitedCells.Add(cell))
+        {
+            AddReward(_explorationReward);
+        }
+    }
+
+    private void ResetAgentPose()
     {
         transform.SetLocalPositionAndRotation(new Vector3(0f, 0.22f, 0f), Quaternion.identity);
-        float randomAngle = Random.Range(0f, 360f);
-        Vector3 randomDirection = Quaternion.Euler(0f, randomAngle, 0f) * Vector3.forward;
-
-        float randomDistance = Random.Range(1f, 2.5f);
-
-        Vector3 targetPosition = transform.localPosition + randomDirection * randomDistance;
-
-        _targetTransform.localPosition = new Vector3(targetPosition.x, 0.22f, targetPosition.z);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (_episodeEnding)
-        {
-            return;
-        }
-
         if (collision.gameObject.CompareTag("Wall"))
         {
-            SetReward(-1f);
-            _renderer.material.color = Color.red;
-            StartCoroutine(EndEpisodeDelayed());
-        }
-        else if (collision.gameObject.CompareTag("Goal"))
-        {
-            GoalReached();
+            AddReward(-_wallCollisionPenalty);
+            StopAllCoroutines();
+            StartCoroutine(FlashWallHit());
         }
     }
 
-    private void GoalReached()
+    private IEnumerator FlashWallHit()
     {
-        SetReward(1f);
-        _renderer.material.color = Color.green;
-        StartCoroutine(EndEpisodeDelayed());
+        _renderer.material.color = Color.red;
+        yield return new WaitForSeconds(_wallFlashDuration);
+        _renderer.material.color = Color.blue;
     }
-
-    private IEnumerator EndEpisodeDelayed()
-    {
-        _episodeEnding = true;
-        yield return new WaitForSeconds(_episodeEndDelay);
-        EndEpisode();
-    }
-
 }
