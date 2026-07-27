8021e44b08fe080688463fdeb522e5ad9b3ba8e8
Leonardo Tagliati <leotagliati56@gmail.com>
2026-07-27 08:07:45 -0300

    feat: seeker movement

--- Assets/Scripts/Seeker/SeekerMovementSystem.cs
+++ Assets/Scripts/Seeker/SeekerMovementSystem.cs
@@ -6,6 +6,7 @@ namespace Assets.Scripts.Seeker
     {
         [SerializeField] private Rigidbody _rigidbody;
         [SerializeField] private float _moveSpeed = 5f;
+        [SerializeField] private float _turnSpeed = 720f;
 
         public void Awake()
         {
@@ -15,8 +16,19 @@ namespace Assets.Scripts.Seeker
 
         public void Move(Vector3 direction)
         {
-            Vector3 movement = _moveSpeed * Time.fixedDeltaTime * direction.normalized;
+            Vector3 flat = new(direction.x, 0f, direction.z);
+            if (flat.sqrMagnitude < 1e-6f)
+                return;
+
+            Vector3 clamped = Vector3.ClampMagnitude(flat, 1f);
+
+            Vector3 movement = _moveSpeed * Time.fixedDeltaTime * clamped;
             _rigidbody.MovePosition(_rigidbody.position + movement);
+
+            Quaternion target = Quaternion.LookRotation(flat.normalized, Vector3.up);
+            Quaternion next = Quaternion.RotateTowards(_rigidbody.rotation, target, _turnSpeed * Time.fixedDeltaTime);
+
+            _rigidbody.MoveRotation(next);
         }
 
         public void ResetMovement()
