e16e26ab02998a14e9e4d520d1af579f2f7f4ea1
Quazmo <leohdmoura@gmail.com>
2026-08-29 17:17:54 -0300

    feat: adicionei o botao de main menu

---
+++ Assets/mainMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
