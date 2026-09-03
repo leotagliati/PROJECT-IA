5944f550496eccd12cca2115ca204cc811f09e50
André Abreu <andrerenato.aabreu@gmail.com>
2026-09-03 17:21:13 -0300

    feat: UI response to inventory

--- Assets/Scripts/Interactions/KeyItem.cs
+++ Assets/Scripts/Interactions/KeyItem.cs
@@ -2,13 +2,8 @@ using UnityEngine;
 
 public class KeyItem : MonoBehaviour, IInteractable
 {
-[SerializeField] private string targetTag = "Key";
-
     public void Interact()
     {        
-        // Garante que só interage se bater com a tag configurada
-        if (!CompareTag(targetTag)) return;
-
         // Procura o inventário do jogador na cena
         PlayerInventory playerInventory = FindFirstObjectByType<PlayerInventory>();
 
---
+++ Assets/Scripts/UI/KeyInventoryUI.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInventoryUI : MonoBehaviour
+{
+    [Header("UI References")]
+    [Tooltip("Prefab of an UI Image with the Key sprite")]
+    [SerializeField] private GameObject keyIconPrefab;
+
+    [Tooltip("Transform that contains the Vertical Layout Group")]
+    [SerializeField] private Transform iconsContainer;
+
+    private readonly List<GameObject> spawnedIcons = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (iconsContainer == null)
+            iconsContainer = transform;
+    }
+
+    private void OnEnable()
+    {
+        PlayerInventory.OnKeyCountChanged += UpdateKeyDisplay;
+    }
+
+    private void OnDisable()
+    {
+        PlayerInventory.OnKeyCountChanged -= UpdateKeyDisplay;
+    }
+
+    private void UpdateKeyDisplay(int totalKeys)
+    {
+        // Se pegou mais chaves do que tem ícones na tela, instancia novos ícones
+        while (spawnedIcons.Count < totalKeys)
+        {
+            GameObject newIcon = Instantiate(keyIconPrefab, iconsContainer);
+            spawnedIcons.Add(newIcon);
+        }
+
+        // Se gastou chaves, remove ícones excedentes
+        while (spawnedIcons.Count > totalKeys)
+        {
+            int lastIndex = spawnedIcons.Count - 1;
+            Destroy(spawnedIcons[lastIndex]);
+            spawnedIcons.RemoveAt(lastIndex);
+        }
+    }
+}
